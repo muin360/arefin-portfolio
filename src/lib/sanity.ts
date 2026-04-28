2a4866352fc980f0cd1f85dd10c763e63877baca
muin360 <74620484+muin360@users.noreply.github.com>
2026-04-28 23:11:19 +0600

    sanity-1

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,14 +1,12 @@
-// /lib/sanity.ts
+import { createClient } from '@sanity/client'
 
-export const projectId = "skiYTgO73Ck2p07URnLWwlq4wwpcRfRWQ4NEQ6gboGz3Lwtsyq5r3XDo8dH51k0IfNvBbPDbmfgULl5lXllEMStRfeTMawWecDGbsSRvy5BAxak5SHOTEowJ854bar3iEQWvNNTU4GBobXqyuOhHenCbp4qWwBj7F2B4ohBTgI6gXDRPJmhH";
-export const dataset = "production";
-export const apiVersion = "2024-01-01";
-
-export const url = `https://${projectId}.api.sanity.io/v${apiVersion}/data/query/${dataset}`;
+export const client = createClient({
+  projectId: "h3kwrsuj",
+  dataset: "production",
+  apiVersion: "2024-01-01",
+  useCdn: false,
+})
 
 export async function getPosts() {
-  const query = encodeURIComponent(`*[_type == "post"]{_id, title}`);
-  const res = await fetch(`${url}?query=${query}`);
-  const data = await res.json();
-  return data.result;
+  return await client.fetch(`*[_type == "post"]{_id, title}`)
 }
